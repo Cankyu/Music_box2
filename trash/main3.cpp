a4c6638f8583c1c0df606d03968bdcdb12e7404d
Monkey D. Luffy <cankyuartdesigns@gmail.com>
2023-06-20 23:52:35 +0200

    Commit 10

---
+++ trash/main3.cpp
@@ -0,0 +1,31 @@
+#include "Music_library.hpp"
+
+int main(int argc, char* argv[]) {
+    MusicLibrary library;
+    library.loadFromJson(argv[1]);
+
+    // Example usage
+    library.searchByTitle("Call out my name");
+
+    Song newSong;
+    newSong.title = "Hallo";
+    newSong.artist = "Jan";
+    newSong.album = "New Album";
+    newSong.year = 2023;
+    newSong.genre = "New Genre";
+    library.addSong(newSong);
+
+    library.removeSong("Kill Bill");
+
+    Song updatedSong;
+    updatedSong.title = "Call out my name";
+    updatedSong.artist = "The Weeknd";
+    updatedSong.album = "My Dear Melancholy";
+    updatedSong.year = 2010;
+    updatedSong.genre = "RnB";
+    library.updateSong("Call out my name", updatedSong);
+
+    library.saveToJson("updated_music_data.json");
+
+    return 0;
+}
